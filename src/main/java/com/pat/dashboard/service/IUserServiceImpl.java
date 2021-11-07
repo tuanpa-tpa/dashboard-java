64b32231794511b5ed7cdc99d40601f22c312572
PAT <tuanpa.tpa@gmail.com>
2021-11-07 12:22:58 +0700

    delete var active in user

--- src/main/java/com/pat/dashboard/service/IUserServiceImpl.java
+++ src/main/java/com/pat/dashboard/service/IUserServiceImpl.java
@@ -5,8 +5,6 @@ import com.pat.dashboard.model.User;
 import com.pat.dashboard.repository.IRoleRepository;
 import com.pat.dashboard.repository.IUserRepository;
 import com.pat.dashboard.web.dto.UserRegistrationDto;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -19,10 +17,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Service
-public class IUserServiceImpl implements IUserService {
+public class IUserServiceImpl implements IUserService{
 
     @Autowired
     private IUserRepository userRepository;
@@ -33,33 +29,20 @@ public class IUserServiceImpl implements IUserService {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
-    @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(s);
-        if (user == null) {
-            throw new UsernameNotFoundException("invalid username or password");
-        }
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesAuthoritites(user.getRoles()) );
+    public IUserServiceImpl(IUserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
     }
 
-    private Collection<? extends GrantedAuthority> mapRolesAuthoritites(Collection<Role> roles) {
-        return roles.stream()
-                .map(r -> new SimpleGrantedAuthority(r.getName()))
-                .collect(Collectors.toList());
-    }
-
-
     @Override
-    public User save(UserRegistrationDto userRegistrationDto) {
+    public User save(UserRegistrationDto registrationDto) {
         User user = new User(
-                userRegistrationDto.getFirstName(),
-                userRegistrationDto.getLastName(),
-                userRegistrationDto.getUsername(),
-                passwordEncoder.encode(userRegistrationDto.getPassword()),
-                true,
-                Arrays.asList(roleRepository.findByName("ADMIN"))
+                registrationDto.getFirstName()
+                ,registrationDto.getLastName()
+                , registrationDto.getUserName()
+                ,passwordEncoder.encode(registrationDto.getPassword())
+                , Arrays.asList(roleRepository.findByName("ADMIN")));
 
-        );
         return userRepository.save(user);
     }
 
@@ -67,4 +50,21 @@ public class IUserServiceImpl implements IUserService {
     public User findByUsername(String username) {
         return userRepository.findByEmail(username);
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
 }
