3fb1cd8c3d068a64b9b1babe92ef6b856648d73f
PAT <tuanpa.tpa@gmail.com>
2021-11-07 12:52:01 +0700

    add search product

--- src/main/java/com/pat/dashboard/repository/IProductRepository.java
+++ src/main/java/com/pat/dashboard/repository/IProductRepository.java
@@ -2,6 +2,17 @@ package com.pat.dashboard.repository;
 
 import com.pat.dashboard.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IProductRepository extends JpaRepository<Product, Long> {
+    @Query("select p from Product p " +
+            "where 1=1" +
+            "and ( upper(p.name) like concat('%', upper(?1), '%') " +
+            "       or upper(p.brand) like concat('%', upper(?1), '%') " +
+            "       or upper(p.madein) like concat('%', upper(?1), '%')" +
+            //"       or upper(pt.name) like concat('%', upper(?1), '%')" +
+            ")")
+    List<Product> searchProduct(String criteria);
 }
